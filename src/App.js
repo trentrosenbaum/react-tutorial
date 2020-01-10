615c9533e46208a0b9be4f1251e115a884d5e308
Trent Rosenbaum <trent.j.rosenbaum@gmail.com>
2020-01-10 12:14:38 +0000

    Adds heading and paragraph to application above table

--- src/App.js
+++ src/App.js
@@ -26,6 +26,8 @@ class App extends Component {
 
         return (
             <div className="container">
+                <h1>React Tutorial</h1>
+                <p>Add a character with a name and a job to the table.</p>
                 <Table characterData={this.state.characters} removeCharacter={this.removeCharacter}/>
                 <Form handleSubmit={this.handleSubmit}/>
             </div>
