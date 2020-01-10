04daf1800efcf1a9875c2e59f7fa722875cba153
Trent Rosenbaum <trent.j.rosenbaum@gmail.com>
2020-01-10 11:21:03 +0000

    Updates

---
+++ precache-manifest.0f7b627172f16ba07c02ce7ffb3b56c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348cc0a7e716c1dcda5a2b3fcaa003d6",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "59a9487a532b3fc40d3c",
+    "url": "/react-tutorial/static/css/main.629b192f.chunk.css"
+  },
+  {
+    "revision": "88af68aa040d5b2c0dc8",
+    "url": "/react-tutorial/static/js/2.c950ad14.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tutorial/static/js/2.c950ad14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59a9487a532b3fc40d3c",
+    "url": "/react-tutorial/static/js/main.2b4ebe57.chunk.js"
+  },
+  {
+    "revision": "da5c02f9a44783ad6b5b",
+    "url": "/react-tutorial/static/js/runtime-main.d2b0c93e.js"
+  }
+]);
