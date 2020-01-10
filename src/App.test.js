a8690c279cb9ffd410ecf345196998940babe225
Trent Rosenbaum <trent.j.rosenbaum@gmail.com>
2020-01-10 17:11:12 +0000

    Update to simple test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,36 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+let container = null;
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+
+it("renders with or without a name", () => {
+  act(() => {
+    render(<App />, container);
+  });
+
+  const actualHeading = container.querySelector('h1');
+  expect(actualHeading.textContent).toBe("React Tutorial");
+
+  const actualParagraph = container.querySelector('p');
+  expect(actualParagraph.textContent).toBe("Add a character with a name and a job to the table.");
+
+
+
 });
